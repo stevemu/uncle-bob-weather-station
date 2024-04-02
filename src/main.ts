82cf756fffb0f5d81d3d7643d9ebffd2ce662820
Steve Mu <skycloud112@gmail.com>
2024-04-01 20:44:35 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,9 @@ import { Nimbus1TemperatureSensor } from './Nimbus1TemperatureSensor.ts';
 import { StreamingOutput } from './StreamingOutput.ts';
 import { TemperatureSensor } from './TemperatureSensor.ts';
 
-const alarmClock = new Nimbus1AlarmClock();
+const ac = new Nimbus1AlarmClock();
 
-const nimbus1TemperatureSensor = new Nimbus1TemperatureSensor();
-const temperatureSensor = new TemperatureSensor(alarmClock, nimbus1TemperatureSensor);
+const ts = new TemperatureSensor(ac, new Nimbus1TemperatureSensor());
+const bps = new BarometricPressureSensor(ac, new Nimbus1BarometricPressureSensor());
 
-const nimbus1BarometricPressureSensor = new Nimbus1BarometricPressureSensor();
-const barometricPressureSensor = new BarometricPressureSensor(
-  alarmClock,
-  nimbus1BarometricPressureSensor,
-);
-
-new StreamingOutput(temperatureSensor, barometricPressureSensor);
+new StreamingOutput(ts, bps);
